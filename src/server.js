8b83ca118adbda36d104910d7466fdb366b9f30e
andersbando-hess <bandersman@gmail.com>
2018-05-01 23:45:07 -0400

    make it better

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
+import * as Polls from './controllers/poll_controller';
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/cs52poll';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
 
 // initialize
 const app = express();
@@ -29,7 +37,34 @@ app.use(bodyParser.json());
 
 // default index route
 app.get('/', (req, res) => {
-  res.send('hi');
+  // we will later be able to get the polls by calling a function, but let's pass in no polls for now
+  Polls.getPolls().then((polls) => {
+    res.render('index', { polls });
+  }).catch((error) => {
+    res.send(`error: ${error}`);
+  });
+});
+
+app.get('/new', (req, res) => {
+  // we will later be able to get the polls by calling a function, but let's pass in no polls for now
+  res.render('new');
+});
+
+app.post('/new', (req, res) => {
+  const newpoll = {
+    text: req.body.text,
+    imageURL: req.body.imageURL,
+  };
+  Polls.createPoll(newpoll).then((poll) => {
+    res.redirect('/');
+  });
+});
+
+app.post('/vote/:id', (req, res) => {
+  const vote = (req.body.vote === 'up');// convert to bool
+  Polls.vote(req.params.id, vote).then((result) => {
+    res.send(result);
+  });
 });
 
 // START THE SERVER
